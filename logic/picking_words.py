d33988aee0a4389268664dd8180c39b9322b9ae8
Hanz Tura <hctura.official@gmail.com>
2018-11-02 19:06:43 +0800

    Picking words using functional programming and modules.

--- logic/picking_words.py
+++ logic/picking_words.py
@@ -1,20 +1,14 @@
-from seeding_words import seeding_words
+from common import read_file, pick_words
 
 filename = str(input())
 dictionary = []
-with open(filename) as f:
-    dictionary = f.read().split()
+
+file = read_file(filename)
+dictionary = file.split()
 
 testcase = int(input())
 for i in range(testcase):
     word_positions = input()
-    array_word_positions = [int(word_positions) for word_positions in word_positions.split()]
-
-    words = ''
-    for j in range(len(array_word_positions)):
-        if words != '':
-            words += " " + str(dictionary[array_word_positions[j]])
-        else:
-            words += str(dictionary[array_word_positions[j]])
+    words = pick_words(word_positions, dictionary)
 
     print(words)
